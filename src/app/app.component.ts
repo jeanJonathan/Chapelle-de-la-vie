7d24bb9cb86a74f34fca60cfebb803cd4e1375e6
Jean Jonathan <jeanjonathank2@gmail.com>
2023-09-17 23:32:42 +0200

    implementation du service pour gerer les donnees de la manne du jour

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ManneService } from './manne.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Chapelle-de-la-Vie-site';
+  manneDuJourData: any[] = [];
+  constructor(private manneService: ManneService) { }
+
+  ngOnInit() {
+    // Utilisez le service pour récupérer les données de la Manne du jour lors de l'initialisation du composant
+    this.manneDuJourData = this.manneService.getManneDuJour();
+  }
 }
+
