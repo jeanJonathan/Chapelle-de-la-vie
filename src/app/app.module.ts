aea130c969df828eff39fff8480f6a39092ae3b8
Jean Jonathan <jeanjonathank2@gmail.com>
2023-09-18 21:17:12 +0200

    Creation du composant mot du conseilDesAdministrateurs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,7 @@ import { ScheduleComponent } from './schedule/schedule.component';
 import { AcceuilComponent } from './acceuil/acceuil.component';
 import { MotDuPasteurComponent } from './mot-du-pasteur/mot-du-pasteur.component';
 import { ConseilDesDiacresComponent } from './conseil-des-diacres/conseil-des-diacres.component';
+import { ConseilDesAdministrateursComponent } from './conseil-des-administrateurs/conseil-des-administrateurs.component';
 
 @NgModule({
   declarations: [
@@ -18,7 +19,8 @@ import { ConseilDesDiacresComponent } from './conseil-des-diacres/conseil-des-di
     ScheduleComponent,
     AcceuilComponent,
     MotDuPasteurComponent,
-    ConseilDesDiacresComponent
+    ConseilDesDiacresComponent,
+    ConseilDesAdministrateursComponent
   ],
   imports: [
     BrowserModule,
